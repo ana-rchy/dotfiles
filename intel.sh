7d19fd45b8723bae95e5b784a8e7e53495872d9e
ana-rchy <85036427+ana-rchy@users.noreply.github.com>
2021-09-07 19:20:00 +0000

    Update intel.sh

--- intel.sh
+++ intel.sh
@@ -10,5 +10,6 @@ sudo cp ~/dotfiles/.config ~/.config &&
 systemctl enable sddm &&
 chmod u+x ~/.config/bspwm/bspwmrc &&
 chmod u+x ~/.config/polybar/launch.sh &&
-echo "\nalias s='killall sxhkd && sxhkd'" >> .bashrc &&
-echo "\nalias nascii='neofetch -L --source ~/dotfiles/oneshot.txt --ascii_colors 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41'" >> .bashrc
+echo "alias s='killall sxhkd && sxhkd'" >> .bashrc &&
+echo "alias nascii='neofetch -L --source ~/dotfiles/oneshot.txt --ascii_colors 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41'" >> .bashrc
+echo "alias ninfo='neofetch --off --colors 14 12 23 13 14'" >> .bashrc
