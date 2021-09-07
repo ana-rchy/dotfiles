30c568cadb93665c32f07be5becee88f3286f524
ana-rchy <85036427+ana-rchy@users.noreply.github.com>
2021-09-07 19:42:15 +0000

    Update intel.sh

--- intel.sh
+++ intel.sh
@@ -1,6 +1,6 @@
 cd ~ &&
 git clone https://aur.archlinux.org/paru-bin.git &&
-cd paru &&
+cd paru-bin &&
 makepkg -si &&
 cd ~ &&
 sudo pacman -S bspwm feh kitty pavucontrol pulseaudio rofi sddm spectacle sxhkd terminus-font xorg xorg-server mesa lib32-mesa xf86-video-intel vulkan-intel &&
