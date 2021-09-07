0df2dd847e1fa037e2a31f3cc906680f598f9ad1
ana-rchy <85036427+ana-rchy@users.noreply.github.com>
2021-09-07 19:36:56 +0000

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -3,7 +3,7 @@ git clone https://aur.archlinux.org/paru.git &&
 cd paru &&
 makepkg -si &&
 cd ~ &&
-sudo pacman -S bspwm feh git kitty pavucontrol pulseaudio rofi sddm spectacle sxhkd terminus-font xorg xorg-server nvidia nvidia-settings lib32-nvidia-utils &&
+sudo pacman -S bspwm feh kitty pavucontrol pulseaudio rofi sddm spectacle sxhkd terminus-font xorg xorg-server nvidia nvidia-settings lib32-nvidia-utils &&
 paru -S cava picom-jonaburg-git polybar terminus-font-ttf &&
 mkdir ~/.config/bspwm/ ~/.config/cava/ ~/.config/kitty/ ~/.config/polybar/ ~/.config/rofi/ ~/.config/sxhkd/ &&
 sudo cp ~/dotfiles/.config ~/.config &&
