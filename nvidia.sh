3ed819ab438c3714434720c1e5ede5fd575278f6
ana-rchy <85036427+ana-rchy@users.noreply.github.com>
2021-09-07 10:52:56 +0100

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -1,9 +1,12 @@
 cd ~ &&
-sudo pacman -S bspwm feh git kitty pavucontrol pulseaudio rofi spectacle sxhkd terminus-font xorg xorg-server nvidia nvidia-settings lib32-nvidia-utils &&
+sudo pacman -S bspwm feh git kitty pavucontrol pulseaudio rofi sddm spectacle sxhkd terminus-font xorg xorg-server nvidia nvidia-settings lib32-nvidia-utils &&
 git clone https://aur.archlinux.org/paru.git &&
 cd paru &&
 makepkg -si &&
 cd ~ &&
 paru -S cava picom-jonaburg-git polybar terminus-font-ttf &&
 sudo cp dotfiles/.config .config/ &&
-echo “alias s=‘killall sxhkd && sxhkd’” > .bashrc
+echo “alias s=‘killall sxhkd && sxhkd’” > .bashrc &&
+systemctl enable sddm &&
+chmod u+x ~/.config/bspwm/bspwmrc &&
+chmod u+x ~/.config/polybar/launch.sh
